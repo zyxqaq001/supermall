78451441f802fc12aff007d9d06a7ae999465614
zyxqaq001 <992128143@qq.com>
2019-12-11 18:32:20 +0800

    请求商品数据

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -16,7 +16,7 @@
   import NavBar from 'components/common/navbar/NavBar';
   import TabControl from 'components/content/tabControl/TabControl'
 
-  import {getHomeMultidata} from "network/home";
+  import {getHomeMultidata,getHomeGoods} from "network/home";
 
   export default {
     name: "Home",
@@ -29,24 +29,40 @@
     },
     data() {
       return {
-        banners: [],
+        banners: [null],
         recommends: [],
         titles:['流行','新款','精选'],
         goods:{
           'pop':{page:0,list:[]},
-          'news':{page:0,list:[]},
+          'new':{page:0,list:[]},
           'sell':{page:0,list:[]},
         }
       }
     },
     created() {
       // 1.请求多个数据
-      getHomeMultidata().then(res => {
-        // this.result = res;
-        this.banners = res.data.banner.list;
-        this.recommends = res.data.recommend.list;
-      })
-    }
+     this.getHomeMultidata()
+     //请求商品数据
+     this.getHomeGoods('pop')
+     this.getHomeGoods('new')
+     this.getHomeGoods('sell')
+    },
+    methods: {
+    //网络请求相关方法
+       getHomeMultidata(){
+          getHomeMultidata().then(res => {
+            this.banners = res.data.banner.list;
+            this.recommends = res.data.recommend.list;
+          })
+       },
+       getHomeGoods(type){
+          const page = this.goods[type].page + 1
+          getHomeGoods(type,page).then(res=>{
+            this.goods[type].list.push(...res.data.list)
+            this.goods[type].page +=1
+          })
+       }
+    },
   }
 </script>
 
