988a201641b7865065a02e571c77ab30a81873c9
zyxqaq001 <992128143@qq.com>
2019-12-12 18:39:07 +0800

    backtop显示和隐藏的功能

--- src/components/common/scroll/Scroll.vue
+++ src/components/common/scroll/Scroll.vue
@@ -14,8 +14,20 @@ export default {
       scroll:null
     }
   },
+  props:{
+    probeType:{
+      type:Number,
+      default:0
+    }
+  },
   mounted() {
+    //创建BScroll对象
     this.scroll = new BScroll(this.$refs.wrapper,{
+      probeType:this.probeType
+    })
+    //监听滚动
+    this.scroll.on('scroll',(position)=>{
+      this.$emit('scroll',position)
     })
   },
   methods: {
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -2,14 +2,14 @@
   <div id="home">
     <nav-bar class="home-nav"><div slot="center">购物街</div></nav-bar>
    
-    <scroll class="content" ref="scroll">
+    <scroll class="content" ref="scroll" :probe-type="3" @scroll="contentScroll">
       <home-swiper :banners="banners"/>
       <recommend-view :recommends="recommends"/>
       <feature-view></feature-view>
       <tab-control class="tab-control" :titles="titles" @tabClick="tabClick"></tab-control>
       <goods-list :goods="showGoods"></goods-list>
     </scroll>
-    <back-top @click.native="backClick"/>
+    <back-top @click.native="backClick" v-show="isShowBackTop"/>
   </div>
 </template>
 
@@ -48,7 +48,8 @@
           'new':{page:0,list:[]},
           'sell':{page:0,list:[]},
         },
-        currentType:'pop'
+        currentType:'pop',
+        isShowBackTop:false
       }
     },
     computed: {
@@ -82,7 +83,9 @@
      backClick(){//监听组件的原生事件时需要加上修饰符native
         this.$refs.scroll.scrollTo(0,0,1000)//通过$refs可以拿到scroll组件里的方法
      },
-
+     contentScroll(position){
+       this.isShowBackTop = (-position.y)>1000
+     },
     //网络请求相关方法
        getHomeMultidata(){  // 请求多个数据
           getHomeMultidata().then(res => {
