0f4d2e72a0a297797651a26340809ffd04bef814
zyxqaq001 <992128143@qq.com>
2019-12-19 16:46:59 +0800

    懒加载图片

--- src/main.js
+++ src/main.js
@@ -4,7 +4,12 @@ import router from './router'
 import store from './store'
 import toast from 'components/common/toast'
 import Fastclick from 'fastclick'
+import VueLazyload from 'vue-lazyload'
 
+//懒加载
+Vue.use(VueLazyload,{
+  loading:require('./assets/img/common/placeholder.png')
+})
 Vue.config.productionTip = false
 
 Vue.prototype.$bus = new Vue()
